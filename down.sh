8595ce9fa362ddbde6219812d835f2ea344371c9
Andy Smith <andy.smith@90poe.io>
2019-03-24 21:21:06 +0000

    Use docker volumes instead of local directory

--- down.sh
+++ down.sh
@@ -4,9 +4,9 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $script_dir/env.sh
 
 echo $dc
-$dc down
 
 if [ "$1" == "purge" ]; then
-  echo delete $script_dir/.kafka_\* ?
-  sudo rm -rf $script_dir/.kafka_*
+  $dc down -v
+else
+  $dc down
 fi
